26cd7cc0a8f85ce8087ed67c52c4affc493e4685
FuniManaga <funanani229@gmail.com>
2023-12-01 22:20:09 +0200

    ce

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -8,7 +8,7 @@ const Main = () => {
         <img className='w-full h-screen object-cover object-left scale-x-[-1]' src='https://th.bing.com/th/id/R.101696699d7e931bddf71141273d3937?rik=oBnfNGABIclwuQ&pid=ImgRaw&r=0' />
         <div className='w-full h-screen absolute top-0 left-0 bg-white/50'>
         <div className='max-w-[700px] m-auto h-full w-full flex flex-col justify-center lg:items-start items-center'>
-            <h1 className='sm:text-5xl text-4xl font-bold text-gray-800'>Welcome to SFFP where future leaders are groomed</h1>
+            <h1 className='sm:text-5xl text-4xl text-centerfont-bold text-gray-800'>Welcome to SFFP where future leaders are groomed</h1>
             <h2 className='flex sm:text-3xl text-2xl pt-4 text-gray-800'>We are creating future
             <TypeAnimation
                 sequence={[
