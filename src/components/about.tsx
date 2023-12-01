be9376be7d9242132f65f7f7e071ab1d284528b4
FuniManaga <funanani229@gmail.com>
2023-12-01 22:47:04 +0200

    g

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,7 +1,6 @@
 
 import ProjectItem from './ProjectItem'
-import propertyImg from '../assets/property.jfif'
-import twitchImg from '../assets/twitch.jfif'
+
 
 export default function Projects() {
   return (
@@ -17,8 +16,8 @@ at <span className='text-[#001b5e]'>SFFP</span>, our mission is to provide quali
         <div className='grid sm:grid-cols-2 gap-12'>
             <ProjectItem img='https://th.bing.com/th/id/OIP.F0tFEhJ09HS7GToSSA8LDgHaE7?rs=1&pid=ImgDetMain' title="Ralson Tshinanne"  />
             <ProjectItem img='https://th.bing.com/th/id/OIP.UO5I7H2eQZ0pSq5RDxA6wAHaHa?rs=1&pid=ImgDetMain' title="Mphaphuli" />
-            <ProjectItem img={propertyImg} title="Phaswana"  />
-            <ProjectItem img={twitchImg} title="Raluswielo"  />
+            <ProjectItem img='https://th.bing.com/th/id/R.34174a26d656d19e053becc51dc71ef2?rik=hB8311M0frDtUw&riu=http%3a%2f%2f3.bp.blogspot.com%2f-Nca-1_92ck8%2fU9eBgtFzUTI%2fAAAAAAAAAbU%2fbzvWcTPTink%2fs1600%2f20140729_093314%2b(2).jpg&ehk=ZU33Hg3a%2fvFsm4R0IWRDUMp7geD8TPucnuwpbr9tkhg%3d&risl=&pid=ImgRaw&r=0' title="Phaswana"  />
+            <ProjectItem img='https://th.bing.com/th/id/R.924e49a6301df6a31efef18f59ab2dd2?rik=HlV9H8n9I3unLw&pid=ImgRaw&r=0' title="Raluswielo"  />
         </div>
     </div>
   )
